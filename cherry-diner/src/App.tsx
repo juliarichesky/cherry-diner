402a51ea484c8e7cf02d46b53079878c58a601f9
julia :) <juliavaleriogs@gmail.com>
2026-04-18 10:02:07 -0300

    feat: implement Layout component with Outlet for nested routing

--- cherry-diner/src/App.tsx
+++ cherry-diner/src/App.tsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Contato from "./pages/Contato/Contato";
+
 const App = () => {
-  return(
+  return (
     <>
-    <h1>Hello, World!</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contato" element={<Contato />} />
+        </Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 export default App;
